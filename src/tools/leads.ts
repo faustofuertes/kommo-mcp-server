dcada5a973859263376df254bff740d4436c0e81
faustofuertes <fausto.fuertes.83@gmail.com>
2025-11-27 09:49:32 -0300

    install dependecies

--- src/tools/leads.ts
+++ src/tools/leads.ts
@@ -14,6 +14,6 @@ export async function getLead(element_id: number) {
 
     } catch (error) {
         console.error("❌ Error en getLead (kommo.service.js):", error);
-        return null;
+        return null
     }
 }
